ba044dd07d22ad61a0b136fe233b813104bbedb3
TAKANO Mitsuhiro <takano32@gmail.com>
2020-02-26 11:54:09 +0900

    use multiprocessing

--- output_decorator.py
+++ output_decorator.py
@@ -1,16 +1,19 @@
 import sys
 import os
+import multiprocessing as mp
 
 # ref. https://qiita.com/taigamikami/items/2713856b9f3c3b90f6fd
+
+lock = mp.Lock()
+
+
 def disable_stderr_output(func):
     def wrapper(*args, **kwargs):
-        tmp_stderr = sys.stderr
-        f = open(os.devnull, 'w')
-        sys.stderr = f
-        func(*args, **kwargs)
-        sys.stderr = tmp_stderr
-        f.close()
-        # print('args: {}, kwargs: {}'.format(args, kwargs))
+        with lock:
+            tmp_stderr = sys.stderr
+            f = open(os.devnull, 'w')
+            sys.stderr = f
+            func(*args, **kwargs)
+            sys.stderr = tmp_stderr
+            f.close()
     return wrapper
-
-
